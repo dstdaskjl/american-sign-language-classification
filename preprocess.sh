05c5d2b17bf84a56835c6179d393544b00eb1b8a
anthony <dstdaskjl@gmail.com>
2022-06-20 17:27:24 -0400

    Split test and train datasets Added conversion from images to csv files

--- preprocess.sh
+++ preprocess.sh
@@ -19,23 +19,51 @@ for filename in *; do
 done
 
 #  Create a directory to gather all files
-mkdir image
+mkdir -p image/all/
 for letter in {A..Z}; do
-  mkdir image/${letter}
+  mkdir image/all/${letter}
 done
 
-#  Move all files to the image directory
+#  Move train dataset to the image directory
 for letter in {A..Z}; do
   ls asl_alphabet_train/asl_alphabet_train/${letter} | sort -R | tail -500 | while read file; do
-    cp asl_alphabet_train/asl_alphabet_train/${letter}/${file} image/${letter}/$(ls image/${letter} | wc -l).jpg
+    cp asl_alphabet_train/asl_alphabet_train/${letter}/${file} image/all/${letter}/$(ls image/all/${letter} | wc -l).jpg
   done
   ls ASL_Dataset/Train/${letter} | sort -R | tail -500 | while read file; do
-    cp ASL_Dataset/Train/${letter}/${file} image/${letter}/$(ls image/${letter} | wc -l).jpg
+    cp ASL_Dataset/Train/${letter}/${file} image/all/${letter}/$(ls image/all/${letter} | wc -l).jpg
   done
   ls dataset/train/${letter} | sort -R | tail -500 | while read file; do
-    cp dataset/train/${letter}/${file} image/${letter}/$(ls image/${letter} | wc -l).jpg
+    cp dataset/train/${letter}/${file} image/all/${letter}/$(ls image/all/${letter} | wc -l).jpg
   done
 done
 
 #  Delete all files/directories except the image directory
 ls | grep -xv image | xargs rm -r
+
+#  Resize images to 200x200
+cd image/all/
+for letter in {A..Z}; do
+  for filename in *; do
+    if [ $(identify -format '%wx%h' ${filename}) != "200x200" ]; then
+      convert ${filename} -resize 200x200 ${filename}
+    fi
+  done
+done
+cd ../
+
+#  Split the dataset into train and test
+mkdir test/
+for letter in {A..Z}; do
+  mkdir test/${letter}
+done
+
+cp -R all/ train/
+for letter in {A..Z}; do
+  ls train/${letter} | sort -R | tail -100 | while read file; do
+    mv train/${letter}/${file} test/${letter}/${file}
+  done
+done
+
+#  Convert images to csv files
+mkdir -p csv/train/ csv/test/
+python3 preprocess.py
