440c9e83cda590a3969f071ccb89ef92b72c0b86
anthony <dstdaskjl@gmail.com>
2022-06-20 22:14:07 -0400

    Changed a directory name. Removed csv conversion.

--- preprocess.sh
+++ preprocess.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/expect -f
 
-pip3 install kaggle
-
 #  You need to create an API token on Kaggle.com
 #  https://github.com/Kaggle/kaggle-api#api-credentials
 
+pip3 install kaggle
 mkdir dataset
 cd dataset
 
 #  Download dataset
-kaggle dataset download -d grassknoted/asl-alphabet
-kaggle dataset download -d kapillondhe/american-sign-language
-kaggle dataset download -d joannracheljacob/american-sign-language-dataset
+kaggle datasets download -d grassknoted/asl-alphabet
+kaggle datasets download -d kapillondhe/american-sign-language
+kaggle datasets download -d joannracheljacob/american-sign-language-dataset
 
 #  Unzip dataset
 for filename in *; do
@@ -19,21 +18,21 @@ for filename in *; do
 done
 
 #  Create a directory to gather all files
-mkdir -p image/all/
+mkdir -p image/train/
 for letter in {A..Z}; do
-  mkdir image/all/${letter}
+  mkdir image/train/${letter}
 done
 
 #  Move train dataset to the image directory
 for letter in {A..Z}; do
   ls asl_alphabet_train/asl_alphabet_train/${letter} | sort -R | tail -500 | while read file; do
-    cp asl_alphabet_train/asl_alphabet_train/${letter}/${file} image/all/${letter}/$(ls image/all/${letter} | wc -l).jpg
+    cp asl_alphabet_train/asl_alphabet_train/${letter}/${file} image/train/${letter}/$(ls image/train/${letter} | wc -l).jpg
   done
   ls ASL_Dataset/Train/${letter} | sort -R | tail -500 | while read file; do
-    cp ASL_Dataset/Train/${letter}/${file} image/all/${letter}/$(ls image/all/${letter} | wc -l).jpg
+    cp ASL_Dataset/Train/${letter}/${file} image/train/${letter}/$(ls image/train/${letter} | wc -l).jpg
   done
   ls dataset/train/${letter} | sort -R | tail -500 | while read file; do
-    cp dataset/train/${letter}/${file} image/all/${letter}/$(ls image/all/${letter} | wc -l).jpg
+    cp dataset/train/${letter}/${file} image/train/${letter}/$(ls image/train/${letter} | wc -l).jpg
   done
 done
 
@@ -41,9 +40,10 @@ done
 ls | grep -xv image | xargs rm -r
 
 #  Resize images to 200x200
-cd image/all/
+cd image/train/
 for letter in {A..Z}; do
-  for filename in *; do
+  echo ${filename}
+  for filename in ${letter}/*; do
     if [ $(identify -format '%wx%h' ${filename}) != "200x200" ]; then
       convert ${filename} -resize 200x200 ${filename}
     fi
@@ -51,19 +51,16 @@ for letter in {A..Z}; do
 done
 cd ../
 
-#  Split the dataset into train and test
+#  Split the dataset into train dataset and test dataset
 mkdir test/
 for letter in {A..Z}; do
   mkdir test/${letter}
 done
 
-cp -R all/ train/
 for letter in {A..Z}; do
-  ls train/${letter} | sort -R | tail -100 | while read file; do
+  ls train/${letter} | sort -R | tail -300 | while read file; do
     mv train/${letter}/${file} test/${letter}/${file}
   done
 done
 
-#  Convert images to csv files
-mkdir -p csv/train/ csv/test/
-python3 preprocess.py
+cd ../../
