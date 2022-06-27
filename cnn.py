0f4e3f09f54d4e6fd0233f69f83d0e434266506a
anthony <dstdaskjl@gmail.com>
2022-06-27 17:05:53 -0400

    Removed the staged saves.

--- cnn.py
+++ cnn.py
@@ -1,11 +1,15 @@
+import sys
+sys.path.append('..')
+
 import random
 import numpy as np
 import traceback
 import pandas as pd
-from filelock import FileLock
+import constant as const
 from tensorflow.keras import models
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
-from path import Path
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+from pylib.path import File, Directory
 
 # Overall CNN format
 # https://www.tensorflow.org/tutorials/images/cnn
@@ -21,97 +25,75 @@ from path import Path
 
 DEFAULT_EPOCH = 1000
 BATCH_SIZE = 64
-
-path = Path()
+file = File()
+directory = Directory()
 
 
 class CNN:
-    def test(self, *data, epoch: int) -> None:
-        path.delete(path.var.result_dir, str(epoch) + '.txt')
+    def test(self, *data) -> None:
+        file.delete(directory.join(const.RESULT_DIR, 'result.txt'))
         x, y = data
-        x = self._reshape_dataset(x=x)
-        model_path = path.join(path.var.model_dir, str(epoch))
-        names = path.listdir(model_path)
 
+        names = directory.list_dir(const.MODEL_DIR)
         for name in names:
             try:
-                print(name, '\n')
-                model = self._load_model(filepath=path.join(model_path, name))
+                print(name)
+                model = self._load_model(filepath=directory.join(const.MODEL_DIR, name))
                 prediction = model.predict(x=x)
-                accuracy = self._accuracy(prediction=prediction, y=y)
-                path.write(
-                    filepath=path.join(path.var.result_dir, str(epoch) + '.txt'),
+                accuracy = self._get_accuracy(prediction=prediction, y=y)
+                file.write_text(
+                    filepath=directory.join(const.RESULT_DIR, 'result.txt'),
                     content=name + ' ' + str(accuracy) + '\n',
                     mode='a'
                 )
-            except Exception as e:
-                print(e)
-                print('\n\n\n')
+                print()
+            except Exception:
+                print(traceback.format_exc())
+                print('\n\n')
 
-    def train(self, *data, epoch: int) -> None:
-        x, y, hidden_activations, output_activations, optimizers, losses = data
-        x = self._reshape_dataset(x=x)
-        x, y = self._shuffle_inputs(x=x, y=y)
+    def train(self, *data) -> None:
+        print('\nStarting training\n')
+        x, y, hidden_activations, output_activations, optimizers, loss_functions, drop_rates = data
 
         for hidden_activation in hidden_activations:
             for output_activation in output_activations:
                 for optimizer in optimizers:
-                    for loss in losses:
-                        name = hidden_activation + '_' + output_activation + '_' + optimizer + '_' + loss
+                    for loss in loss_functions:
+                        for drop_rate in drop_rates:
+                            name = hidden_activation + '_' + output_activation + '_' + optimizer + '_' + loss + '_' + drop_rate
 
-                        # The previous epoch model does not exist.
-                        if epoch != 1000 and not self._model_exists(name=name, epoch=epoch - 1000):
-                            continue
-                        # The model already exists.
-                        if self._model_exists(name=name, epoch=epoch):
-                            continue
-                        # The model is being created by another process.
-                        if self._name_was_used(name=name + str(epoch)):
-                            continue
-
-                        try:
-                            print(name, '\n')
-                            self._model(
-                                hidden_activation,
-                                output_activation,
-                                optimizer,
-                                loss,
-                                name=name,
-                                epoch=epoch,
-                                x=x,
-                                y=y,
-                            )
-                        except Exception:
-                            path.write(
-                                filepath=path.join(path.var.exception_dir, name + str(epoch) + '.txt'),
-                                content=traceback.format_exc()
-                            )
-
-    def _accuracy(self, prediction: np.array, y: np.array) -> float:
-        count = 0
-        for i in range(len(y)):
-            if prediction.argmax(axis=-1)[i] == y[i][0]:
-                count += 1
-        accuracy = round(count / len(y) * 100, 3)
-        return accuracy
+                            try:
+                                print(name)
+                                model = self._create_model(hidden_activation, output_activation, optimizer, loss, drop_rate, x=x)
+                                callback = EarlyStopping(monitor='loss', patience=3)
+                                self._fit(model=model, callback=callback, name=name, x=x, y=y)
+                                print()
+                            except Exception:
+                                file.write_text(
+                                    filepath=directory.join(const.EXCEPTION_DIR, name + '.txt'),
+                                    content=traceback.format_exc()
+                                )
 
     def _create_model(self, *func, x: np.array) -> models.Sequential:
-        hidden_activation, output_activation, optimizer, loss = func
+        hidden_activation, output_activation, optimizer, loss, drop_rate = func
         model = models.Sequential()
 
         # first layer
-        model.add(Conv2D(32, (3, 3), padding='same', activation=hidden_activation, input_shape=x.shape[1:]))
+        model.add(Conv2D(16, (3, 3), activation=hidden_activation, input_shape=x.shape[1:]))
         model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(drop_rate))
 
         # second layer
-        model.add(Conv2D(64, (3, 3), padding='same', activation=hidden_activation))
+        model.add(Conv2D(32, (3, 3), activation=hidden_activation))
         model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(drop_rate))
 
         # third layer
-        model.add(Conv2D(128, (3, 3), padding='same', activation=hidden_activation))
+        model.add(Conv2D(64, (3, 3), activation=hidden_activation))
+        model.add(Flatten())
+        model.add(Dropout(drop_rate))
 
         # output layer
-        model.add(Flatten())
         model.add(Dense(128, activation=hidden_activation))
         model.add(Dense(26, activation=output_activation))  # 26 letters
 
@@ -122,51 +104,24 @@ class CNN:
         )
         return model
 
-    def _load_model(self, filepath: str) -> models.Sequential:
-        return models.load_model(filepath=filepath)
-
-    def _model(self, *func, name: str, epoch: int, x: np.array, y: np.array) -> None:
-        model = self._create_model(*func, x=x) if epoch == 1000 else self._load_model(filepath=path.join(path.var.model_dir, str(epoch - 1000), name))
-        history = model.fit(x, y, batch_size=BATCH_SIZE, epochs=DEFAULT_EPOCH, validation_split=.2)
+    def _fit(self, model: models.Sequential, callback: EarlyStopping, name: str, x: np.array, y: np.array) -> None:
+        history = model.fit(x, y, batch_size=BATCH_SIZE, epochs=DEFAULT_EPOCH, validation_split=.2, callbacks=[callback])
         self._save_history(
-            filepath=path.join(path.var.history_dir, str(epoch), name + '.json'),
+            filepath=directory.join(const.HISTORY_DIR, name + '.json'),
             history_df=pd.DataFrame(history.history)
         )
-        model.save(path.join(path.var.model_dir, str(epoch), name))
+        model.save(directory.join(const.MODEL_DIR, name))
 
-    def _model_exists(self, name: str, epoch: int) -> bool:
-        for model_name in path.listdir(path.join(path.var.model_dir, str(epoch))):
-            if model_name == name:
-                return True
-        return False
+    def _get_accuracy(self, prediction: np.array, y: np.array) -> float:
+        count = 0
+        for i in range(len(y)):
+            if prediction.argmax(axis=-1)[i] == y[i]:
+                count += 1
+        accuracy = round(count / len(y) * 100, 3)
+        return accuracy
 
-    def _name_was_used(self, name: str) -> bool:
-        with FileLock(path.var.used_lock):
-            names = path.read(filepath=path.var.used_filepath)
-            if name not in names:
-                path.write(filepath=path.var.used_filepath, content=name + '\n')
-        return True if name in names else False
-
-    def _reshape_dataset(self, x: np.array) -> np.array:
-        # Normalizing
-        x = x / 255.0
-
-        # Add an image channel. We use a mono-channel
-        image_count = len(x)
-        pixel_count = len(x[0])
-        channel_count = 1
-        x = x.reshape(image_count, pixel_count, pixel_count, channel_count)
-
-        return x
-
-    # https://github.com/keras-team/keras/issues/4298#issuecomment-258947029
-    def _shuffle_inputs(self, x: np.array, y: np.array) -> tuple:
-        temp = list(zip(x, y))
-        random.shuffle(temp)
-        x, y = zip(*temp)
-        del temp
-        x, y = np.array(x), np.array(y)
-        return x, y
+    def _load_model(self, filepath: str) -> models.Sequential:
+        return models.load_model(filepath=filepath)
 
     def _save_history(self, filepath: str, history_df: pd.DataFrame) -> None:
         with open(filepath, mode='w') as f:
