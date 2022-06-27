bae1b1e18334015add98de84aefc540ae6712749
anthony <dstdaskjl@gmail.com>
2022-06-27 17:06:31 -0400

    Removed preprocess and postprocess functions.

--- data.py
+++ data.py
@@ -1,118 +1,59 @@
+import sys
+sys.path.append('..')
+
 import csv
 import math
 import random
 import json
 import numpy as np
+import constant as const
 from PIL import Image
+from pylib.path import File, Directory
+from pylib.image import Image
 
-from path import Path
 
-categories = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+file = File()
+directory = Directory()
+image = Image()
+
+ROW_COUNT = 50
+COL_COUNT = 50
+CHANNEL_COUNT = 3
+CATEGORIES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
                       'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 
-class Data(Path):
-    # Convert image files to csv files.
-    def preprocess(self) -> None:
-        # If directory does not exist, create one.
-        if not self.exists('Training'):
-            self.create('Training')
-        if not self.exists('Test'):
-            self.create('Test')
-
-        # If directory is not empty, delete all files.
-        if not self.is_empty('Training'):
-            self.delete('Training')
-            self.create('Training')
-        if not self.is_empty('Test'):
-            self.delete('Test')
-            self.create('Test')
-
-        for category in categories:
-            print(category)
-            cat_path = self.join('Dataset', category)
-            training_path = self.join('Training', category + '.csv')
-            test_path = self.join('Test', category + '.csv')
-
-            training_dataset, test_dataset = self._split_training_test(self.listdir(cat_path))
-            for img_filename in training_dataset:
-                img_path = self.join('Dataset', category, img_filename)
-                self._img_2_csv(img_path=img_path, csv_path=training_path)
-            for img_filename in test_dataset:
-                img_path = self.join('Dataset', category, img_filename)
-                self._img_2_csv(img_path=img_path, csv_path=test_path)
-
-        print('\nThe dataset has been preprocessed.\n')
-
-    def load_training_dataset(self) -> tuple:
-        dataset, labels = self._load_dataset(directory='Training')
-        hidden_activations = ['relu', 'sigmoid', 'tanh']
-        output_activations = ['linear', 'sigmoid', 'softmax']
-        optimizers = ['sgd', 'rmsprop', 'adam', 'adadelta', 'adagrad']
-        losses = ['sparse_categorical_crossentropy']
-        return dataset, labels, hidden_activations, output_activations, optimizers, losses
-
-    def load_training_result(self, epoch: int) -> dict:
-        training_result = dict()
-        filepath = self.join(self.var.history_dir, str(epoch))
-        for filename in sorted(self.listdir(filepath)):
-            d = json.load(open(self.join(filepath, filename)))
-
-            # keys = ['loss', 'accuracy', 'val_loss', 'val_accuracy']
-            final_result = dict()
-            for k, v in d.items():
-                max_epoch = max([e for e in v.keys()])
-                final_result[k] = v[str(max_epoch)]
-            training_result[filename] = final_result
-        return training_result
+class Data:
+    def load_train_dataset(self) -> tuple:
+        dataset, label = self._load_dataset(ds_path=const.TRAIN_DIR)
+        dataset = self._normalize(x=dataset)
+        dataset, label = self._shuffle_inputs(x=dataset, y=label)
+        return dataset, label
 
     def load_test_dataset(self) -> tuple:
-        dataset, labels = self._load_dataset(directory='Test')
-        return dataset, labels
+        dataset, label = self._load_dataset(ds_path=const.TEST_DIR)
+        dataset = self._normalize(x=dataset)
+        dataset, label = self._shuffle_inputs(x=dataset, y=label)
+        return dataset, label
 
-    def load_test_result(self, epoch: int) -> dict:
-        content = self.read(filepath=self.join(self.var.result_dir, str(epoch) + '.txt'))
-        content_dict = dict()
-        for line in content:
-            content_dict[line.split(' ')[0]] = float(line.split(' ')[1])
-        content_dict = dict(reversed(sorted(content_dict.items(), key=lambda item: item[1])))
-        return content_dict
+    def _load_dataset(self, ds_path: str) -> tuple:
+        dataset, label = list(), list()
+        for cat in const.CATEGORIES:
+            print(cat, 'has been loaded.')
+            for filename in directory.list_dir(ds_path, cat):
+                img_arr = image.to_array(src=directory.join(ds_path, cat, filename))
+                dataset.append(img_arr)
+            label.extend([ord(cat) - 65] * len(directory.list_dir(ds_path, cat)))
+        return np.array(dataset), np.array(label)
 
-    def _load_dataset(self, directory) -> tuple:
-        dataset = list()
-        labels = list()
+    def _normalize(self, x: np.ndarray) -> np.ndarray:
+        return x / 255.0
 
-        # A=0 ... Z=25
-        for category in categories:
-            csv_path = self.join(directory, category + '.csv')
-            cat_dataset = self._csv_2_list(csv_path=csv_path)
-            cat_label = [[ord(category) - 65]] * len(cat_dataset)
-            dataset.extend(cat_dataset)
-            labels.extend(cat_label)
-            print('\n' + category + ' has been loaded.\n')
-
-        return np.array(dataset), np.array(labels)
-
-    def _csv_2_list(self, csv_path: str, mode='r') -> list:
-        images = list()
-        with open(csv_path, mode) as f:
-            reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-            for row in reader:
-                # Convert 1d array to 2d array
-                pixel_count = int(math.sqrt(len(row)))
-                image_2d = np.reshape(row, (-1, pixel_count))
-                images.append(image_2d)
-        return images
-
-    def _img_2_csv(self, img_path: str, csv_path: str, mode='a') -> None:
-        img = Image.open(img_path)
-        img_arr = np.array(img).flatten()
-        with open(csv_path, mode) as f:
-            write = csv.writer(f)
-            write.writerow(img_arr)
-
-    def _split_training_test(self, dataset: np.array) -> tuple:
-        random.shuffle(dataset)
-        training = dataset[:int(len(dataset) * 0.8)]
-        test = dataset[int(len(dataset) * 0.8):]
-        return training, test
+    # https://github.com/keras-team/keras/issues/4298#issuecomment-258947029
+    def _shuffle_inputs(self, x: np.array, y: np.array) -> tuple:
+        temp = list(zip(x, y))
+        random.shuffle(temp)
+        x, y = zip(*temp)
+        del temp
+        x, y = np.array(x), np.array(y)
+        return x, y
