1b9e6dc7904950cf69b83a2471612120fa32cb1a
anthony <dstdaskjl@gmail.com>
2022-06-27 17:05:32 -0400

    Removed the staged saves.

--- cleanup.py
+++ cleanup.py
@@ -1,31 +1,14 @@
-import variable as var
+import sys
+sys.path.append('..')
+
+import constant as const
 from pylib.path import File, Directory
 
-FILE = File()
-DIR = Directory()
+file = File()
+directory = Directory()
 
-DIR.delete('Exception')
-DIR.delete('Shared')
-DIR.delete('research.py')
-
-DIR.create('Exception')
-DIR.create('Shared')
-
-if not DIR.exists('Result'):
-    DIR.create('Result')
-
-if not DIR.exists('History'):
-    DIR.create('History')
-    for i in range(1, 6):
-        if not DIR.exists('History', str(i * 1000)):
-            DIR.create('History', str(i * 1000))
-
-if not DIR.exists('Model'):
-    DIR.create('Model')
-    for i in range(1, 6):
-        if not DIR.exists('Model', str(i * 1000)):
-            DIR.create('Model', str(i * 1000))
-
-DIR.create(var.USED_FILEPATH)
-DIR.create(var.ALIVE_FILEPATH)
-FILE.write_text(var.STATUS_FILEPATH, var.ALIVE)
+directory.delete(const.RESULT_DIR)
+directory.create(const.RESULT_DIR)
+directory.create(const.MODEL_DIR)
+directory.create(const.HISTORY_DIR)
+directory.create(const.EXCEPTION_DIR)
