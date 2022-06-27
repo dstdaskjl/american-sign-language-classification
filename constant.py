34873ca2bc32fe91da2bc39f4414e563b7486151
anthony <dstdaskjl@gmail.com>
2022-06-27 17:07:06 -0400

    Initial commit.

---
+++ constant.py
@@ -0,0 +1,20 @@
+READ_LOCK = 'shared/read'
+STATUS_LOCK = 'shared/status'
+USED_LOCK = 'shared/used'
+ALIVE_LOCK = 'shared/alive'
+
+STATUS_FILEPATH = 'shared/status.txt'
+USED_FILEPATH = 'shared/used.txt'
+ALIVE_FILEPATH = 'shared/alive.txt'
+
+RESULT_DIR = 'result/'
+MODEL_DIR = 'result/model/'
+HISTORY_DIR = 'result/history/'
+EXCEPTION_DIR = 'result/exception/'
+TRAIN_DIR = 'dataset/train/'
+TEST_DIR = 'dataset/test/'
+
+ALIVE = 'ALIVE'
+KILLED = 'KILLED'
+CATEGORIES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                      'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
